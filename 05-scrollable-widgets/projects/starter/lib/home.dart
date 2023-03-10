08bf3bd7ee2814231f22a5034fdcdb9d414b29ab
Ling Xiao Chok <ling-xiao.chok.wg@renesas.com>
2023-03-10 22:31:54 +0800

    chapter 5 WIP

--- 05-scrollable-widgets/projects/starter/lib/home.dart
+++ 05-scrollable-widgets/projects/starter/lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -11,10 +14,8 @@ class HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with ExploreScreen
-    Container(color: Colors.white),
-    // TODO: Replace with RecipesScreen
-    Container(color: Colors.green),
+    ExploreScreen(),
+    RecipesScreen(),
     Container(color: Colors.blue),
   ];
 
---
+++ 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  // 1
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final recipes = snapshot.data?.todayRecipes ?? [];
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: recipes),
+              const SizedBox(height: 10),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
